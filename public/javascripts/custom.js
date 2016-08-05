0eaa2069baff142c16dc162f20847afcce37a3ec
lisiur <lisiurday@gmail.com>
2016-08-05 12:24:18 +0800

    发送Ajax后，再次执行事件监听

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -38,6 +38,26 @@ $("document").ready(function() {
         console.log(status);
         $('.body').html(data.html);
         $('.tj span').html(data.tongji.now +'/' + data.tongji.original);
+        // 添加监听事件
+        $('span.ip').each(function() {
+          var ip = $(this).text().split('/')[0];
+          var ip_b = ip.split('.')[1];
+          var ip_b_2 = parseInt(ip_b).toString(2);
+          ip_b_2 = (function(str) {
+            var b_ip_2 = str;
+            var b_ip_2_len = b_ip_2.length;
+            for(var iii=0 ; iii<8-b_ip_2_len; iii++){
+              b_ip_2 = '0' + b_ip_2 ;
+            }
+            return b_ip_2;
+          })(ip_b_2);
+          $(this).after("<div class='ip-hint'>"+ ip_b_2 +"</div>");
+          $(this).parent().children('.ip-hint').hide();
+          var self = this;
+          $(self).click(function () {
+            $(self).parent().children('.ip-hint').toggle(200);
+          });
+        });
       });
     });
   });
